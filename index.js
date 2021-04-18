c7c15bdbc7c35330f38d413200f6650c597332a3
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-04-18 07:14:43 +0600

    add admin

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ client.connect(err => {
 
     console.log("Database connected Successfully")
 
+    // admin 
+    const adminsCollection = client.db("tech-soldiers").collection("admin");
+
+    app.get('/addAdmin/:email', async (req, res) => {
+        const adminEmail = req.params.email
+        const adminCreated = await adminsCollection.insertOne({ adminEmail });
+        if (adminCreated.insertedCount > 0) {
+            res.send(adminCreated.ops[0])
+        }
+    })
+
     //  users Section //
 
     const usersCollection = client.db("tech-soldiers").collection("users");
@@ -46,13 +57,33 @@ client.connect(err => {
         res.send({ message: 'update Successfully' })
     });
 
-    app.post('/addUser', (req, res) => {
+    app.post('/addUser', async (req, res) => {
         const user = req.body;
         const { email } = user;
         usersCollection.findOne({ email }, (err, data) => {
-            console.log(data, 'from server');
+
             if (data) {
-                res.send(data)
+                const { isSignedIn, name, email, } = data;
+                console.log(email)
+                adminsCollection.findOne({ adminEmail: email }, (err, adminData) => {
+                    if (adminData) {
+
+                        const makeAdmin = async (email) => {
+                            await usersCollection.updateOne({ "email": email }, { $set: { "role": 'admin' } })
+                        }
+                        makeAdmin(email);
+                        const payload = {
+                            isSignedIn,
+                            name,
+                            email,
+                            role: "admin"
+                        }
+                        res.send(payload);
+                    } else {
+                        res.send(data);
+                    }
+                })
+
             } else {
                 usersCollection.insertOne(user)
                     .then(result => {
@@ -64,6 +95,7 @@ client.connect(err => {
         })
 
     })
+
 
     //  services Section //
     const servicesCollection = client.db("tech-soldiers").collection("services");
@@ -91,6 +123,16 @@ client.connect(err => {
             })
     })
 
+    // Delete Services
+    app.get('/deleteService/:id', async (req, res) => {
+        const id = new ObjectId(req.params.id);
+        console.log(id)
+        const deleteService = await servicesCollection.deleteOne({ _id: id });
+        console.log(deleteService)
+        if (deleteService.deletedCount > 0) {
+            res.send({ message: 'Delete Successfully' })
+        }
+    })
     // Order Section    //
 
     const orderCollection = client.db("tech-soldiers").collection("order");
@@ -107,10 +149,36 @@ client.connect(err => {
     });
 
     app.get("/getOrders", (req, res) => {
+
         orderCollection.find({}).toArray((err, documents) => {
             res.send(documents);
         });
     });
+
+
+    // 
+    app.get("/orderByEmail/:email", (req, res) => {
+        const email = req.params.email
+        orderCollection.find({ email: email }).toArray((err, documents) => {
+            res.send(documents);
+        });
+    });
+
+    // update order status 
+
+    app.post('/orderStatus/:id', async (req, res) => {
+        try {
+            const id = new ObjectId(req.params.id);
+            const { status } = req.body
+            console.log(status)
+            await orderCollection.updateOne({ _id: id }, { $set: { "status": status } });
+            res.send({ message: 'Update Successfully ' });
+        } catch (err) {
+            res.send(err)
+        }
+    });
+
+
     //  reviews section //
 
     const reviewsCollection = client.db("tech-soldiers").collection("reviews");
